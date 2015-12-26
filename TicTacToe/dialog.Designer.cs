f84392c019f3f2708c190e6200a52872668bd192
Burak <burak_okur@hotmail.com>
2015-12-26 17:48:56 +0200

    Simple TicTacToe Game.

---
+++ TicTacToe/dialog.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace TicTacToe
+{
+    partial class dialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.b11 = new System.Windows.Forms.Button();
+            this.b12 = new System.Windows.Forms.Button();
+            this.b13 = new System.Windows.Forms.Button();
+            this.b21 = new System.Windows.Forms.Button();
+            this.b22 = new System.Windows.Forms.Button();
+            this.b23 = new System.Windows.Forms.Button();
+            this.b33 = new System.Windows.Forms.Button();
+            this.b32 = new System.Windows.Forms.Button();
+            this.b31 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.sen_score = new System.Windows.Forms.Label();
+            this.ben_score = new System.Windows.Forms.Label();
+            this.status = new System.Windows.Forms.Label();
+            this.restart = new System.Windows.Forms.Button();
+            this.babymode = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // b11
+            // 
+            this.b11.Font = new System.Drawing.Font("Tahoma", 18.33962F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b11.Location = new System.Drawing.Point(12, 15);
+            this.b11.Name = "b11";
+            this.b11.Size = new System.Drawing.Size(80, 80);
+            this.b11.TabIndex = 0;
+            this.b11.Text = "X";
+            this.b11.UseVisualStyleBackColor = true;
+            this.b11.Click += new System.EventHandler(this.gamebut_Click);
+            // 
+            // b12
+            // 
+            this.b12.Font = new System.Drawing.Font("Tahoma", 18.33962F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b12.Location = new System.Drawing.Point(98, 15);
+            this.b12.Name = "b12";
+            this.b12.Size = new System.Drawing.Size(80, 80);
+            this.b12.TabIndex = 1;
+            this.b12.Text = "X";
+            this.b12.UseVisualStyleBackColor = true;
+            this.b12.Click += new System.EventHandler(this.gamebut_Click);
+            // 
+            // b13
+            // 
+            this.b13.Font = new System.Drawing.Font("Tahoma", 18.33962F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b13.Location = new System.Drawing.Point(184, 15);
+            this.b13.Name = "b13";
+            this.b13.Size = new System.Drawing.Size(80, 80);
+            this.b13.TabIndex = 2;
+            this.b13.Text = "X";
+            this.b13.UseVisualStyleBackColor = true;
+            this.b13.Click += new System.EventHandler(this.gamebut_Click);
+            // 
+            // b21
+            // 
+            this.b21.Font = new System.Drawing.Font("Tahoma", 18.33962F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b21.Location = new System.Drawing.Point(12, 101);
+            this.b21.Name = "b21";
+            this.b21.Size = new System.Drawing.Size(80, 80);
+            this.b21.TabIndex = 3;
+            this.b21.Text = "X";
+            this.b21.UseVisualStyleBackColor = true;
+            this.b21.Click += new System.EventHandler(this.gamebut_Click);
+            // 
+            // b22
+            // 
+            this.b22.Font = new System.Drawing.Font("Tahoma", 18.33962F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b22.Location = new System.Drawing.Point(98, 101);
+            this.b22.Name = "b22";
+            this.b22.Size = new System.Drawing.Size(80, 80);
+            this.b22.TabIndex = 4;
+            this.b22.Text = "X";
+            this.b22.UseVisualStyleBackColor = true;
+            this.b22.Click += new System.EventHandler(this.gamebut_Click);
+            // 
+            // b23
+            // 
+            this.b23.Font = new System.Drawing.Font("Tahoma", 18.33962F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b23.Location = new System.Drawing.Point(184, 101);
+            this.b23.Name = "b23";
+            this.b23.Size = new System.Drawing.Size(80, 80);
+            this.b23.TabIndex = 5;
+            this.b23.Text = "X";
+            this.b23.UseVisualStyleBackColor = true;
+            this.b23.Click += new System.EventHandler(this.gamebut_Click);
+            // 
+            // b33
+            // 
+            this.b33.Font = new System.Drawing.Font("Tahoma", 18.33962F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b33.Location = new System.Drawing.Point(184, 187);
+            this.b33.Name = "b33";
+            this.b33.Size = new System.Drawing.Size(80, 80);
+            this.b33.TabIndex = 8;
+            this.b33.Text = "X";
+            this.b33.UseVisualStyleBackColor = true;
+            this.b33.Click += new System.EventHandler(this.gamebut_Click);
+            // 
+            // b32
+            // 
+            this.b32.Font = new System.Drawing.Font("Tahoma", 18.33962F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b32.Location = new System.Drawing.Point(98, 187);
+            this.b32.Name = "b32";
+            this.b32.Size = new System.Drawing.Size(80, 80);
+            this.b32.TabIndex = 7;
+            this.b32.Text = "X";
+            this.b32.UseVisualStyleBackColor = true;
+            this.b32.Click += new System.EventHandler(this.gamebut_Click);
+            // 
+            // b31
+            // 
+            this.b31.Font = new System.Drawing.Font("Tahoma", 18.33962F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b31.Location = new System.Drawing.Point(12, 187);
+            this.b31.Name = "b31";
+            this.b31.Size = new System.Drawing.Size(80, 80);
+            this.b31.TabIndex = 6;
+            this.b31.Text = "X";
+            this.b31.UseVisualStyleBackColor = true;
+            this.b31.Click += new System.EventHandler(this.gamebut_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Monaco", 10.18868F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(285, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Sen";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Monaco", 10.18868F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(285, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ben";
+            // 
+            // sen_score
+            // 
+            this.sen_score.AutoSize = true;
+            this.sen_score.Font = new System.Drawing.Font("Monaco", 10.18868F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sen_score.Location = new System.Drawing.Point(300, 75);
+            this.sen_score.Name = "sen_score";
+            this.sen_score.Size = new System.Drawing.Size(19, 20);
+            this.sen_score.TabIndex = 1;
+            this.sen_score.Text = "0";
+            // 
+            // ben_score
+            // 
+            this.ben_score.AutoSize = true;
+            this.ben_score.Font = new System.Drawing.Font("Monaco", 10.18868F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ben_score.Location = new System.Drawing.Point(300, 161);
+            this.ben_score.Name = "ben_score";
+            this.ben_score.Size = new System.Drawing.Size(19, 20);
+            this.ben_score.TabIndex = 1;
+            this.ben_score.Text = "0";
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.Font = new System.Drawing.Font("Monaco", 10.18868F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.status.Location = new System.Drawing.Point(14, 275);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(0, 20);
+            this.status.TabIndex = 1;
+            // 
+            // restart
+            // 
+            this.restart.Font = new System.Drawing.Font("Monaco", 8.150944F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.restart.Location = new System.Drawing.Point(270, 231);
+            this.restart.Name = "restart";
+            this.restart.Size = new System.Drawing.Size(75, 23);
+            this.restart.TabIndex = 9;
+            this.restart.Text = "Tekrar";
+            this.restart.UseVisualStyleBackColor = true;
+            this.restart.Click += new System.EventHandler(this.restart_Click);
+            // 
+            // babymode
+            // 
+            this.babymode.AutoSize = true;
+            this.babymode.Location = new System.Drawing.Point(270, 206);
+            this.babymode.Name = "babymode";
+            this.babymode.Size = new System.Drawing.Size(90, 19);
+            this.babymode.TabIndex = 10;
+            this.babymode.Text = "Bebe Modu";
+            this.babymode.UseVisualStyleBackColor = true;
+            // 
+            // dialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(361, 309);
+            this.Controls.Add(this.babymode);
+            this.Controls.Add(this.restart);
+            this.Controls.Add(this.ben_score);
+            this.Controls.Add(this.sen_score);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.b31);
+            this.Controls.Add(this.b32);
+            this.Controls.Add(this.b33);
+            this.Controls.Add(this.b23);
+            this.Controls.Add(this.b22);
+            this.Controls.Add(this.b21);
+            this.Controls.Add(this.b13);
+            this.Controls.Add(this.b12);
+            this.Controls.Add(this.b11);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "dialog";
+            this.Text = "Tic Tac Toe";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b11;
+        private System.Windows.Forms.Button b12;
+        private System.Windows.Forms.Button b13;
+        private System.Windows.Forms.Button b21;
+        private System.Windows.Forms.Button b22;
+        private System.Windows.Forms.Button b23;
+        private System.Windows.Forms.Button b33;
+        private System.Windows.Forms.Button b32;
+        private System.Windows.Forms.Button b31;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label sen_score;
+        private System.Windows.Forms.Label ben_score;
+        private System.Windows.Forms.Label status;
+        private System.Windows.Forms.Button restart;
+        private System.Windows.Forms.CheckBox babymode;
+    }
+}
+
---
+++ TicTacToe/dialog.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+
+namespace TicTacToe
+{
+    public partial class dialog : Form
+    {
+        private Button[] array;
+        public dialog()
+        {
+            InitializeComponent();
+
+            array = new[] {b11, b12, b13, b21, b22, b23, b31, b32, b33};
+            isItMyTurn = false;
+            baseColor = b11.BackColor;
+            restart_Click(null,null);
+        }
+
+        private Color baseColor;
+
+        private bool isItMyTurn;
+
+        public enum ECase
+        {
+            X,O,NULL
+        }
+
+        bool checkIfOver() => array.All(button => string.IsNullOrEmpty(button.Text) != true);
+
+        void resetBoard()
+        {
+            Array.ForEach(array,a => a.Text = "");
+        }
+
+        void Log(string str)
+        {
+            status.Text = str;
+        }
+
+        private void gamebut_Click(object sender, EventArgs e)
+        {
+            if (gameOver) return;
+            Button b = (Button) sender;
+            if (string.IsNullOrEmpty(b.Text) == false) return;
+            b.Text = @"O";
+            checkWhoWins();
+            letAIShowItsMagic();
+        }
+
+        
+
+        private int scoreBen, scoreSen;
+        private bool gameOver;
+        private void checkWhoWins()
+        {
+            if (gameOver) return;
+            gameOver = true;
+            ECase winner = checkFor();
+            switch (winner)
+            {
+            case ECase.X:
+                scoreBen ++;
+                ben_score.Text = scoreBen.ToString();
+                Log("Ben kazandım...");
+                break;
+            case ECase.O:
+                scoreSen++;
+                sen_score.Text = scoreSen.ToString();
+                if (babymode.Checked) bebeScore++;
+                Log("Sen kazandın, tebrikler.");
+                break;
+            case ECase.NULL:
+                if (checkIfOver())
+                    Log("Oyun berabere bitti");
+                else gameOver = false;
+                break;
+            }
+        }
+
+        private int bebeScore;
+        private int[][] checkArray =
+        {
+            new[] {0,1,2},
+            new[] {3,4,5},
+            new[] {6,7,8},
+            new[] {0,3,6},
+            new[] {1,4,7},
+            new[] {2,5,8},
+            new[] {0,4,8},
+            new[] {2,4,6}
+        };
+
+        ECase checkFor()
+        {
+            foreach (int[] ints in checkArray)
+            {
+                bool lineup = true;
+                string first = array[ints[0]].Text;
+                if (string.IsNullOrEmpty(first)) continue;
+                for (int i = 1; i < ints.Length; i++) { lineup = lineup && array[ints[i]].Text.Equals(first); }
+                if (lineup == false) continue;
+                foreach (var i in ints) { array[i].BackColor = Color.Red; }
+                return ECase.O.ToString().Equals(array[ints[0]].Text) ? ECase.O : ECase.X;
+            }
+            return ECase.NULL;
+        }
+
+
+        private void restart_Click(object sender, EventArgs e)
+        {
+            resetBoard();
+            gameOver = false;
+            isItMyTurn = !isItMyTurn;
+            foreach (var a in array) { a.BackColor = baseColor; }
+            if (isItMyTurn) letAIShowItsMagic();
+            Log(isItMyTurn ? "Bu el ben başladım" : "Sen başlıyorsun");
+            if (isItMyTurn) return;
+            if (scoreBen < 3 || babymode.Checked) return;
+            if (scoreSen == 0) Log("Umarım bu eli alırsın benden.");
+            else if(scoreSen - bebeScore == 0) Log("Bebe modu kapalıykende kazanabilirsin.");
+        }
+
+        Random random = new Random();
+        void letAIShowItsMagic()
+        {
+            if (checkIfOver() || gameOver) return;
+            if (babymode.Checked)
+            {
+                while (true)
+                {
+                    int i = random.Next(array.Length);
+                    if (!string.IsNullOrEmpty(array[i].Text)) continue;
+                    array[i].Text = @"X";
+                    break;
+                }
+            }
+            else
+            {
+                do
+                {
+                    if (checkIf1MoveLeft()) break;
+                    if (checkIfCenterEmpty()) break;
+                    if (checkIfBestCornerAvailable()) break;
+                    JustMakeAMove();
+                } while (false);
+            }
+            checkWhoWins();            
+        }
+
+        bool checkIfSame(int a, int b, int c)
+        {
+            if (string.IsNullOrEmpty(array[c].Text) == false) return false;
+            if (string.IsNullOrEmpty(array[a].Text)) return false;
+            if (!array[a].Text.Equals(array[b].Text)) return false;
+            array[c].Text = @"X";
+            return true;
+        } 
+
+        bool checkIf1MoveLeft()
+        {
+            foreach (var ca in checkArray)
+            {
+                if (checkIfSame(ca[0], ca[1], ca[2])) return true;
+                if (checkIfSame(ca[0], ca[2], ca[1])) return true;
+                if (checkIfSame(ca[2], ca[1], ca[0])) return true;
+            }
+            return false;
+        }
+
+
+        bool checkIfCenterEmpty()
+        {
+            if (string.IsNullOrEmpty(array[4].Text) == false) return false;
+            array[4].Text = @"X";
+            return true;
+        }
+
+
+        bool isEnemy(params int[] x) => x.All(i => array[i].Text.Equals("O"));
+
+        bool checkOneOfThem(params int[] places)
+        {
+            foreach (var place in places) {
+                if (!string.IsNullOrEmpty(array[place].Text)) continue;
+                array[place].Text = @"X";
+                return true;
+            }
+            return false;
+        }
+
+        bool AllEmpty(params int[] a) => a.All(i => string.IsNullOrEmpty(array[i].Text));
+
+        bool checkIfBestCornerAvailable()
+        {
+            if (isEnemy(0,8) || isEnemy(2,6)) { return checkOneOfThem(1, 3, 5, 7); }
+
+            if (isEnemy(3, 1) && AllEmpty(0, 2, 6)) return checkOneOfThem(0, 2, 6);
+            if (isEnemy(5, 1) && AllEmpty(0, 2, 8)) return checkOneOfThem(2, 0, 8);
+            if (isEnemy(3, 7) && AllEmpty(0, 8, 6)) return checkOneOfThem(6, 0, 8);
+            if (isEnemy(7, 5) && AllEmpty(8, 2, 6)) return checkOneOfThem(8, 2, 6);
+
+            if (isEnemy(0, 5) && AllEmpty(1,2)) return checkOneOfThem(1, 2);
+            if (isEnemy(1, 8) && AllEmpty(5, 2)) return checkOneOfThem(2, 5);
+            if (isEnemy(2, 7) && AllEmpty(5, 8)) return checkOneOfThem(5, 8);
+            if (isEnemy(3, 2) && AllEmpty(1, 0)) return checkOneOfThem(0, 1);
+            if (isEnemy(5, 6) && AllEmpty(7, 8)) return checkOneOfThem(7, 8);
+            if (isEnemy(6, 1) && AllEmpty(3, 0)) return checkOneOfThem(3, 0);
+            if (isEnemy(7, 0) && AllEmpty(3, 6)) return checkOneOfThem(3, 6);
+            if (isEnemy(8, 3) && AllEmpty(7, 6)) return checkOneOfThem(6, 7);
+
+           
+
+            int[] d = {0, 2, 6, 8};
+            foreach (var i in d) {
+                if (!string.IsNullOrEmpty(array[i].Text)) continue;
+                array[i].Text = @"X";
+                return true;
+            }
+            return false;
+        }
+
+
+        void JustMakeAMove()
+        {
+            foreach (var a in array) {
+                if (!string.IsNullOrEmpty(a.Text)) continue;
+                a.Text = @"X";
+                return;
+            }
+        }
+
+    }
+}
